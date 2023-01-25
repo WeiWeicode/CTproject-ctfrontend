549c9807f5d825bc23926116162daf77c811a3f9
Jia-wei Jiang <asdf0359@gmail.com>
2023-01-25 08:05:26 +0800

    20230125

---
+++ src/components/router/chart.vue
@@ -0,0 +1,32 @@
+<template>
+  
+
+    <v-container fluid>
+        <v-view v-if="tab === 1">
+            <stopwatch />
+        </v-view>
+
+        <v-view v-if="tab === 2">
+            <FTP20min />
+        </v-view>
+
+      <statisticalCard />
+    </v-container>
+  </template>
+    
+    
+    
+    <script>
+  // 引用統計卡片
+  import stopwatch from "../chart/stopwatch.vue";
+  import FTP20min from "../chart/FTP20min.vue";
+  
+  export default {
+    name: "chart",
+  
+    components: {
+        stopwatch,
+        FTP20min,
+    },
+  };
+  </script>
